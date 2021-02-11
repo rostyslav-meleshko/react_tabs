74a7a404e8733260ff496dcd918655baa9809e02
Rostyslav Meleshko <meleshkorostyslav@gmail.com>
2021-02-12 01:14:01 +0200

    Updates

---
+++ precache-manifest.91b18d0eaf6d8279c5e1b68fb23046d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0476af09f996a9e80b80f9515edb5335",
+    "url": "./index.html"
+  },
+  {
+    "revision": "94929351c949cab9b32a",
+    "url": "./static/css/main.26fed1ea.chunk.css"
+  },
+  {
+    "revision": "4b964fc25ecc0b658447",
+    "url": "./static/js/2.18166693.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.18166693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94929351c949cab9b32a",
+    "url": "./static/js/main.80354491.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
